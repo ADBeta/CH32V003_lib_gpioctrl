138aea9dc960f4a923bdc88476a393c19e09e794
ADBeta <ashleydylanrudd@gmail.com>
2024-06-09 13:26:51 +0100

    INPUT working

--- testing/gpiotest.c
+++ testing/gpiotest.c
@@ -1,3 +1,10 @@
+/******************************************************************************
+* lib_GPIOCTRL demonstation and test ground
+* Provides examples of Digital Read/Write, TODO: Analog Read / Write
+*
+******************************************************************************/
+
+
 #include "ch32v003fun.h"
 #include <stdio.h>
 
@@ -15,8 +22,14 @@ int main()
 
 	while(1)
 	{
-		gpio_digital_write(GPIO_PD6, GPIO_HIGH);
-		gpio_digital_write(GPIO_PD6, GPIO_LOW);
+		/*
+		uint8_t val = (uint8_t)gpio_digital_read(GPIO_PD6);
+		printf("%d val\n", val);
+
+		Delay_Ms(250);
+		*/
+
+
 
 		gpio_digital_write(GPIO_PD6, GPIO_HIGH);
 		gpio_digital_write(GPIO_PD6, GPIO_LOW);
@@ -33,6 +46,11 @@ int main()
 		gpio_digital_write(GPIO_PD6, GPIO_HIGH);
 		gpio_digital_write(GPIO_PD6, GPIO_LOW);
 
+		gpio_digital_write(GPIO_PD6, GPIO_HIGH);
+		gpio_digital_write(GPIO_PD6, GPIO_LOW);
+
+		gpio_digital_write(GPIO_PD6, GPIO_HIGH);
+		gpio_digital_write(GPIO_PD6, GPIO_LOW);
 
 
 	}
